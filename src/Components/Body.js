e342c75b9ed4b25631329697ac25048ee28e4026
janyajaiswal <jaiswaljanya@gmail.com>
2024-02-09 19:17:13 +0530

    Basic outline

---
+++ src/Components/Body.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import banner from '/Users/janyajaiswal/Desktop/SDP/currycares/currycares/src/Components/banner.png';
+
+export default function Body() {
+  return (
+    <div>
+      <img src={banner} alt="Banner" />
+    </div>
+  );
+}
