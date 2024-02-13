f81b9476138b4bf0eba8aab6870cb189c6dedf04
janyajaiswal <jaiswaljanya@gmail.com>
2024-02-13 10:26:22 +0530

    added css file for footer
    modified:   src/Components/Footer.js
    new file:   src/Components/footer.css

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from 'react';
+import './Footer.css';
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="footer">
+      <div className="footer-content">
+        <h3>Save with Curry Cares!</h3>
+        <p>Order now and enjoy great savings on delicious meals.</p>
+        <div className="social-links">
+          <a href="https://www.instagram.com/currycares"><i className="fab fa-instagram"></i></a>
+          <a href="https://www.facebook.com/currycares"><i className="fab fa-facebook"></i></a>
+          <a href="https://twitter.com/currycares"><i className="fab fa-twitter"></i></a>
+        </div>
+      </div>
+    </footer>
+  );
 }
