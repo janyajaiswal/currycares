2117865e9beb15bb74eacbe97a18edfb50ad414a
janyajaiswal <jaiswaljanya@gmail.com>
2024-02-14 10:09:57 +0530

    updates

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import Body from '../Components/Body'
 import Footer from '../Components/Footer'
 
 export default function Home() {
   return (
       <div>
           <div><Navbar /></div>
-          <div>Body</div>
+          <div><Body/></div>
           <div><Footer/></div>
 
     </div>
