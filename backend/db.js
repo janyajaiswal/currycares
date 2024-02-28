9fb733a439c5b65264e2e5582794f658756c4139
janyajaiswal <jaiswaljanya@gmail.com>
2024-02-28 10:20:36 +0530

    connection to mongodb atlas

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://jaiswaljanya:SDP123›@currycares.rsaraqd.mongodb.net/?retryWrites=true&w=majority&appName=CurryCares';
+const mongoURI = 'mongodb+srv://jaiswaljanya:SDP123›@currycares.rsaraqd.mongodb.net/CurryCares?retryWrites=true&w=majority&appName=CurryCares';
 
 const connectToDB = async () => {
   try {
-    await mongoose.connect(mongoURI, {
-    });
+    await mongoose.connect(mongoURI, {});
     console.log('Connected to MongoDB');
+
+    const collection = mongoose.connection.db.collection("BackendDB");
+    const fetchedData = await collection.find({}).toArray();
+    console.log(fetchedData);
   } catch (error) {
     console.error('Failed to connect to MongoDB', error);
   }
